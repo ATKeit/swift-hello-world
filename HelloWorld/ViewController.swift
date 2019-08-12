5049e3262c9416e292cb28cb7f0e9c1961bbf2fc
ATHeart <b.angelthink@gmail.com>
2019-08-12 19:50:52 +0900

    Show Hello World
    - Added button
    - Added Labels

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello , " + txtName.text!
+    }
+    
 }
 
